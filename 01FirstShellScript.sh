3c019769153d71e38f63d0d5c2932fce130a6290
Sahil Naik <nsahil992@gmail.com>
2024-09-08 10:57:27 +0530

    feat: First Shell-Script

---
+++ 01FirstShellScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash    
+# This is called as SHEBANG. It tells the operating system which interpreter to use to run the script.
+
+# We can write comment in the same format
+
+<< comment
+This is a multi-line comment.
+We can write any number of lines in this section.
+Primarily it is used to explain what our Shell Script does.
+comment
+
+echo "Shell-Script is amazing"
