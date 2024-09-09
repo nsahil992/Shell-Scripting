9e18aee4f67ef8bdae54b73a8a0596a16001dbb2
Sahil Naik <nsahil992@gmail.com>
2024-09-09 11:16:56 +0530

    feat: Arguments

---
+++ 04Arguments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+car_name=$1
+
+echo "I am currently driving a $car_name"
+
+
+car_color=$2
+
+echo "The color of my $car_name is $car_color"
+
+
+car_speed=$3
+
+echo "I drive my $car_color $car_name around $car_speed kmph"
