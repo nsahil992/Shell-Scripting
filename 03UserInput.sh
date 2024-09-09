9af0891cba69597a7b9cae32f0c3e09fde933bbb
Sahil Naik <nsahil992@gmail.com>
2024-09-09 10:37:24 +0530

    feat: Taking input from user

---
+++ 03UserInput.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter what are you learning: "
+read learning
+
+echo "Your are learning $learning"
+
+read -p "Enter your favourite color: " color
+
+echo "Your favourite color is $color"
+
+
