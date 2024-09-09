568cdef1a513f7e2113786ea95f7516a9742cdbc
Sahil Naik <nsahil992@gmail.com>
2024-09-09 13:29:24 +0530

    feat: If else conditions

---
+++ 06IfElse.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read -p "Enter your rank: " rank
+
+if [ $rank -eq 1 ]; 
+then
+	echo "Congratulations! You won a 🥇 medal"
+elif [ $rank -eq 2 ];
+then 
+	echo "Congratulations! You won a 🥈 medal"
+elif [ $rank -eq 3 ];
+then
+	echo "Congratulations! You won a 🥉 medal"
+else
+	echo "You tried hard! Well done for putting efforts"
+fi
+
+
+
+my_age=21
+
+if [ $my_age -ge 18 ];
+then
+	echo "You are eligible to drive"
+else
+	echo "Sorry, You are not eligible"
+fi
