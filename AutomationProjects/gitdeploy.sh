55250dd98b333c834a7c9cff063580f3bc9e9eb5
Sahil Naik <nsahil992@gmail.com>
2024-09-09 18:20:21 +0530

    feat: Git Deploy Automation Script

--- AutomationProjects/gitdeploy.sh
+++ AutomationProjects/gitdeploy.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
 
-echo "Enter the keyword: "
-read keyword
+function deploy_code_on_github() {
 
-if [ "$keyword" = "deploy" ];
-then
-	echo "Enter the commit message: "
-	read commit_message
+	echo "Enter the file name you want to deploy to the Github: "
+	read file_name
 
-	git add .
+	if [ ! -f "$file_name" ];
+	then
+	echo "File $file_name not found"
+	fi
+
+	if [ ! -d ".git" ];
+	then
+	echo "This is not a Git repository. Initializing a new git repository..."
+        git init
+	fi
+
+	git_remote_url=$(git remote get-url origin 2>/dev/null)
+    	if [ -z "$git_url" ];
+	then
+        echo "No remote URL found. Please enter the remote URL for 'git remote add origin':"
+        read remote_url
+        git remote add origin "$remote_url"
+        echo "Remote URL added: $remote_url"
+    	else
+        echo "Remote URL already set: $git_remote_url"
+    	fi
+
+	echo "********** FETCHING.....**********"
+	git remote -v
+
+	echo "********** GIT ADD **********"
+	git add "$file_name"
+
+	echo "********** GIT COMMIT **********"
+	read -p "Enter the commit message: " commit_message
 	git commit -m "$commit_message"
+
+	echo "********** PUSHING.....**********"
 	git push origin master
 
-	if [ $0? -eq 0 ];
+	if [ $? -eq 0 ];
 	then
-		echo "Changes has been pushed successfully to the master branch"
-	else
-		echo "Failed to push changes"
-	fi
+	echo "File $file_name pushed successfully!"
+    else
+        echo "Failed to push $file_name to GitHub. Please check the error message above."
+    fi
+}
+
+if [ "$1" = "deploy" ];
+then
+    deploy_to_github
 else
-	echo "Incorrect keyword"
-	exit 1
+    echo "Invalid command. Type 'deploy' to execute the script."
 fi
+
+
